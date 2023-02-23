6f5a83c9df040d4c92f1f85e6ba2b5be1bba6f9a
Enamulali <enamul97@icloud.com>
2023-02-23 19:07:10 +0000

    handle POST request, add event

--- routes/events.js
+++ routes/events.js
@@ -6,7 +6,21 @@ const eventsRouter = express.Router();
 eventsRouter.get("/", async (req, res) => {
   try {
     const events = await Event.find();
-    res.json(events);
+    res.status(200).send(events);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+eventsRouter.post("/", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.start || !req.body.end) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const postedEvent = await Event.create(req.body);
+    res.status(201).send(postedEvent);
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Internal server error" });
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const eventsRouter = require("./routes/events");
 
 const app = express();
 
+app.use(express.json());
+app.use("/events", eventsRouter);
+
 const port = 9090;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
-
-app.use("/events", eventsRouter);
