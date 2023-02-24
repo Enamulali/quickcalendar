ea2659c96cbceed451c1a8b453d54b4d5a751ef5
Enamulali <enamul97@icloud.com>
2023-02-24 20:36:29 +0000

    handle 404 error, event not found

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { validateId } = require("./middleware/errors");
 const eventsRouter = require("./routes/events");
 
 const app = express();
 
+//Middleware
 app.use(express.json());
+app.use("/events/:id", validateId);
+
+//Routes
 app.use("/events", eventsRouter);
 
 const port = 9090;
