7284227fc37d3a0970461863c91b5a9010050a0b
Enamulali <enamul97@icloud.com>
2023-03-10 23:16:01 +0000

    Create login route

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Login from "./routes/login";
 import Root from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
