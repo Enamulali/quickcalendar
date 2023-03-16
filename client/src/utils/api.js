4406f6cabda067b4b3675addc2c634981af933f5
Enamulali <enamul97@icloud.com>
2023-03-16 21:24:26 +0000

    get All events

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,6 +4,15 @@ const api = axios.create({
   baseURL: "http://localhost:9090/",
 });
 
+// Set the authorization header
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export const loginUser = (input) => {
   return api.post("/auth/login", input).then((res) => {
     console.log(res.data);
@@ -13,6 +22,12 @@ export const loginUser = (input) => {
 
 export const registerUser = (input) => {
   return api.post("/auth/", input).then((res) => {
+    return res.data;
+  });
+};
+
+export const getAllEvents = () => {
+  return api.get("/events").then((res) => {
     console.log(res.data);
     return res.data;
   });
