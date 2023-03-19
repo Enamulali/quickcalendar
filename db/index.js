7e24d4daec1cde24d74a3b8ba11770b9532217ed
Enamulali <enamul97@icloud.com>
2023-03-19 12:42:23 +0000

    Add run seed dev prompt

--- db/index.js
+++ db/index.js
@@ -6,24 +6,38 @@ const User = require("./models/user");
 const Event = require("./models/event");
 const Calendar = require("./models/calendar");
 const { seed } = require("./seeds/seed");
+const inquirer = require("inquirer");
 
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    process.env.NODE_ENV === "development" &&
-      console.log("Connected to database");
-
-    mongoose.connection.once("open", async () => {
-      await seed();
+async function main() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-  })
-  .catch((err) => {
-    console.log(err);
+    console.log("Connected to database");
+
+    let answer = "y";
+    while (answer.toLowerCase() === "y") {
+      answer = (
+        await inquirer.prompt({
+          type: "input",
+          name: "continue",
+          message: "Seed database again? (y/n)",
+        })
+      ).continue;
+      if (answer.toLowerCase() === "y") {
+        await seed();
+      }
+    }
+
+    mongoose.disconnect();
+  } catch (error) {
+    console.error(error);
     process.exit(1);
-  });
+  }
+}
+
+main();
 
 module.exports = {
   User,
