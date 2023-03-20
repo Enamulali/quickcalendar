6d45a161878f7935d28423876dcc934236138032
Enamulali <enamul97@icloud.com>
2023-03-20 22:07:09 +0000

    Add toggle to dashboard

--- client/src/routes/Dashboard.jsx
+++ client/src/routes/Dashboard.jsx
@@ -1,7 +1,34 @@
-import { getAllEvents } from "../utils/api";
+import { useState } from "react";
+import SliderToggle from "../components/SliderToggle";
+import CalendarEvents from "../components/CalendarEvents";
+import ListEvents from "../components/ListEvents";
+import { Col, Container, Row } from "react-bootstrap";
 
-const Dashboard = () => {
-  return <div>Dashboard</div>;
+const Dashboard = ({ events }) => {
+  const [view, setView] = useState("list");
+
+  const handleViewChange = (isChecked) => {
+    setView(isChecked ? "calendar" : "list");
+  };
+
+  return (
+    <Container fluid>
+      <Row>
+        <Col>
+          <SliderToggle onChange={handleViewChange} setView={setView} />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          {view === "list" ? (
+            <ListEvents events={events} />
+          ) : (
+            <CalendarEvents events={events} />
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Dashboard;
